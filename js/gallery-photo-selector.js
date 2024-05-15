521aa916a38706dbe00f25d70490a0b041a81207
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-05-15 15:06:49 +0300

    Исправлена ошибка при переключении изображений в галерее

--- js/gallery-photo-selector.js
+++ js/gallery-photo-selector.js
@@ -1,10 +1,11 @@
 const activePhoto = document.querySelector('.active-photo');
-const previews = document.querySelectorAll('.gallery-previews a');
+const previews = document.querySelectorAll('.preview-item');
 
 for (let preview of previews) {
     preview.addEventListener('click', (evt) => {
         evt.preventDefault();
-        activePhoto.src = preview.href;
+        const previewLink = preview.querySelector('a');
+        activePhoto.src = previewLink.href;
         const activePreview = document.querySelector('.active-item');
         activePreview.classList.remove('active-item');
         preview.classList.add('active-item');
